41598eab5acd535f376c90924dfedb8d1c52ddec
GSB Acceptance <gsb-acceptance@local.invalid>
2026-09-24 15:21:38 +0800

    GSB task 33 product B

--- tests/test_platform.py
+++ tests/test_platform.py
@@ -12,6 +12,13 @@ def test_api_live_and_plan():
 def test_cobs_round_trip():
  data=b'a\x00b\x00';assert decode(encode(data))==data
 
+def test_cobs_block_boundary_round_trip():
+ for n in [0,1,253,254,255,256,508,509,510,762,763,764]:
+  for data in (b'\x01'*n, bytes((i%257)&0xFF for i in range(n)), bytes(range(256))*(n//256)+bytes(range(n%256))):
+   enc=encode(data)
+   assert enc.count(0)==1 and enc[-1]==0
+   assert decode(enc)==data
+
 def test_varint_round_trip():
  for n in [0,1,127,128,999999]:assert vd(ve(n))[0]==n
 
